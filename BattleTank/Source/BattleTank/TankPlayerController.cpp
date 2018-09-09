b3dc85691942588f80f649db8512c1c977d4c782
DESKTOP-39RBVJF\Dell <serjthomas0@hotmail.com>
2018-09-09 18:07:25 +0400

    BT23 Using Virtual and Override

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,9 +2,21 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing the Tank"))
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Possesing: %s"), *ControlledTank->GetName())
+	}
+}
 
-ATank* ATankPlayerController::GetControlledTank() const {
-
+// returning a pawn with type tank so that the player controller known the tank.
+ATank* ATankPlayerController::GetControlledTank() const
+{
 	return Cast<ATank>(GetPawn());
 }
 
