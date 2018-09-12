94677121ed16064e701337fc20d43c0e4270daeb
DESKTOP-39RBVJF\Dell <serjthomas0@hotmail.com>
2018-09-12 14:27:08 +0400

    BT26 Add Tick() to PlayerController

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -14,10 +14,24 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick( DeltaTime );
+	AimTowardsCrosshair();
+}
+
 /// get the tank that tankplayercontroller is a parent of the (TankPlayerController_BP)
 /// that possesing from (the game mode).
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn()); /// we get the pawn that the player controller is possesing and treat it as a tank so that the functionality in the tank.cpp change it.
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+	// Get world location if linetrace through crosshair
+	// if it hits the landscape
+		// Tell controlled tank to aim at this point
 }
 
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -16,7 +16,10 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
-	ATank* GetControlledTank() const;
-	
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	ATank* GetControlledTank() const;
+	// Start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects the world.
+	void AimTowardsCrosshair();
 };
